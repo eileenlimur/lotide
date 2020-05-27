366d7754bb5a83b5fe79ee5f5827ada28457a0a8
Eileen Li <eileenlimur@gmail.com>
2020-05-27 20:05:04 +0000

    created countOnly.js, a function that counts chosen values within an array as indicated by an object

---
+++ countOnly.js
@@ -0,0 +1,55 @@
+//assertion function
+
+const assertEqual = function(actual, expected) {
+  const successMessage = `🏅🏅🏅Assertion Passed: ${actual} === ${expected}`;
+  const failureMessage = `🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`;
+  if (actual === expected) {
+    console.log(successMessage);
+  } else if (actual !== expected) {
+    console.log(failureMessage);
+  }
+};
+
+//actual function
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (!results[item]) {
+        results[item] = 1;
+      } else {
+        results[item] += 1;
+      }
+    }
+  }
+
+  return results;
+}
+
+//testing functions with variables
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+console.log(result1);
+/*
+expected: Jason 1 Fang 2
+*/
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
