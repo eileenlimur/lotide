f079d5c2b31fefc08d446f5905d100a68b2e26b7
Eileen Li <eileenlimur@gmail.com>
2020-05-27 22:05:37 +0000

    created letterPositions.js, a function to produce an object displaying indexes of each appearance of each letter in a sentence

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const assertEqual = function(array1, array2) {
+const assertArraysEqual = function(array1, array2) {
   const successMessage = `🏅🏅🏅Assertion Passed: ${array1} === ${array2}`;
   const failureMessage = `🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`;
   
@@ -12,14 +12,17 @@ const assertEqual = function(array1, array2) {
 
 const eqArrays = function(array1, array2) {
   let equals = true;
+  
   if (array1.length !== array2.length) {
     equals = false;
   }
+  
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       equals = false;
     }
   }
+  
   return equals;
 };
 
---
+++ letterPositions.js
@@ -0,0 +1,58 @@
+//assertion functions
+
+const assertArraysEqual = function(array1, array2) {
+  const successMessage = `🏅🏅🏅Assertion Passed: ${array1} === ${array2}`;
+  const failureMessage = `🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`;
+  
+  if (eqArrays(array1, array2)) {
+    console.log(successMessage);
+  } else {
+    console.log(failureMessage);
+  }
+  
+};
+
+const eqArrays = function(array1, array2) {
+  let equals = true;
+  
+  if (array1.length !== array2.length) {
+    equals = false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      equals = false;
+    }
+  }
+  
+  return equals;
+};
+
+//actual functions
+
+const letterPositions = function(sentence) {
+  const results = {};
+  const simpleSentence = sentence.toLowerCase();
+  
+  for (let i = 0; i < simpleSentence.length; i++) {
+    if (simpleSentence[i] !== " ") {
+      if (!results[simpleSentence[i]]) {
+        results[simpleSentence[i]] = [];
+        results[simpleSentence[i]].push(i);
+      } else {
+        results[simpleSentence[i]].push(i);
+      }
+    }
+  }
+  
+  return results;
+}
+
+//testing
+
+const letterPositionsHello = letterPositions("hello");
+
+assertArraysEqual(letterPositionsHello["h"],[0]);
+assertArraysEqual(letterPositionsHello["e"],[1]);
+assertArraysEqual(letterPositionsHello["l"],[2, 3]);
+assertArraysEqual(letterPositionsHello["o"],[4]);
