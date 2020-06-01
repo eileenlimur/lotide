8608d11abb2734563dfce71f6133e6ba73cfad08
Eileen Li <eileenlimur@gmail.com>
2020-06-01 14:44:32 -0400

    began refactoring files using module.exports and require(). Did so for head.js and tail.js. Created test folder to place tests inside

--- head.js
+++ head.js
@@ -3,15 +3,4 @@ const head = (array) => {
   return firstElement;
 };
 
-const assertEqual = function(actual, expected) {
-  const successMessage = `🏅🏅🏅Assertion Passed: ${actual} === ${expected}`;
-  const failureMessage = `🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`;
-  if (actual === expected) {
-    console.log(successMessage);
-  } else if (actual !== expected) {
-    console.log(failureMessage);
-  }
-};
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello"]), "Hello");
+module.exports = head;
